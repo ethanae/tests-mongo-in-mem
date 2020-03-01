e06b897cfd930a2830a375a50802693c079f6eaa
ethanae <eemandien@protonmail.com>
2020-03-01 20:20:45 +0200

    - min working pkg versions for mongod and mongoose - specify memory mongo bin version - reference pre-installed mongod binary in packages

--- packages/package-1/test/first.spec.ts
+++ packages/package-1/test/first.spec.ts
@@ -1,26 +1,40 @@
-import { connect } from 'mongoose';
+import * as mongoose from 'mongoose';
+import 'mocha';
+import { expect } from 'chai';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 const log = console.log;
 
-const mongod = new MongoMemoryServer();
+let mongoServer: MongoMemoryServer;
 
-(async () => {
-  const uri = await mongod.getUri();
-  const port = await mongod.getPort();
-  const dbPath = await mongod.getDbPath();
-  const dbName = await mongod.getDbName();
-
-  log(`
-    URI: ${uri}
-    PORT: ${port}
-    DB_PATH: ${dbPath}
-    DB_NAME: ${dbName}
-  `);
-
-  connect(uri, {}, err => {
-    log(err)
+before(async () => {
+  mongoServer = new MongoMemoryServer({
+    // @ts-ignore
+    useNewUrlParser: true,
+    useMongoClient: false
   });
-})()
+  log('starting server')
 
+  const mongoUri = await mongoServer.getUri();
+  log('mongoUri', mongoUri)
+  mongoose.connect(mongoUri);
+  log('CONNECTED')
+});
 
-mongod.getInstanceInfo(); // return Object with instance data
+after(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+describe('...', () => {
+  it('...',  async () => {
+    log('test 2')
+    const User = mongoose.model('User', new mongoose.Schema({ name: String }));
+    log('User model', User.modelName);
+    await User.create(new User({
+      name: 'bab'
+    }));
+
+    const result = await User.findOne({name: 'bob'}).exec();
+    expect(result).to.be.null;
+  });
+});
